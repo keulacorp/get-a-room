7b9d6e340abf673e4e62d120861aa3422608481c
Jan Berg <jan.berg@vincit.fi>
2024-07-08 10:22:58 +0300

    Small changes to adding ordinal to number. Now handles the undefined case.

--- frontend/src/util/commonUtils.ts
+++ frontend/src/util/commonUtils.ts
@@ -1,11 +1,20 @@
 /**
  * Returns the ordinal suffix of a given number.
  * @param num - The number to get the ordinal suffix for, supports string and number
- * @returns The number with its ordinal suffix.
+ * @returns The number with its ordinal suffix. Or "-" when undefined.
  */
 export function getNumberWithOrdinalSuffix(num: any): string {
-    if (typeof num !== 'string' && typeof num !== 'number') {
-        throw new Error('Invalid arguments, supports only strings or numbers!');
+    switch (typeof num) {
+        case 'undefined':
+            return '-';
+        case 'number':
+            break;
+        case 'string':
+            break;
+        default:
+            throw new Error(
+                'Invalid arguments, supports only strings or numbers!'
+            );
     }
     const value = (typeof num === 'number' ? num : Number.parseInt(num)) % 100;
     const suffixes = ['th', 'st', 'nd', 'rd'];
