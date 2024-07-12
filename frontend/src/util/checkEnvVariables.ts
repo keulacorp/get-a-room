c8adaf12af958715adae4501e32a4f42ea4c8398
Jan Berg <jan.berg@vincit.fi>
2024-07-12 10:16:42 +0300

    Added support for Microsoft Clarity for gathering analytics data

--- frontend/src/util/checkEnvVariables.ts
+++ frontend/src/util/checkEnvVariables.ts
@@ -1,7 +1,7 @@
 import GETAROOM_ENV from './getARoomEnv';
 
 export const checkEnvVariables = () => {
-    const { VITE_REACT_APP_SERVER_KEY } = GETAROOM_ENV();
+    const { VITE_REACT_APP_SERVER_KEY, VITE_CLARITY_ID } = GETAROOM_ENV();
 
     if (!VITE_REACT_APP_SERVER_KEY) {
         throw new Error('Application server key not set');
