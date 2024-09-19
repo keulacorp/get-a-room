1b8eddd41e718ae5334b7f3da91ece6810f4f05f
Ville Pynttäri <ville.pynttari@vincit.fi>
2024-09-19 15:28:26 +0300

    No rooms found card styles

--- frontend/src/components/RoomCard/NoRoomsCard.tsx
+++ frontend/src/components/RoomCard/NoRoomsCard.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Typography, styled } from '@mui/material';
+import { Box, styled, Typography } from '@mui/material';
 import { CustomCard, GridContainer, Row } from './RoomCard';
 
 const CardTitleText = styled(Typography)(({ theme }) => ({
@@ -8,6 +8,14 @@ const CardTitleText = styled(Typography)(({ theme }) => ({
     fontSize: '24px',
     fontWeight: 2,
     lineHeight: '24px'
+}));
+const CardTextWrapper = styled(Box)(({ theme }) => ({
+    textAlign: 'left',
+    lineHeight: 'normal',
+    wordWrap: 'break-word', // Wrap long words to the next line
+    whiteSpace: 'normal', // Allow text to wrap normally
+    wordBreak: 'break-word', // Break long words if necessary
+    overflowY: 'auto'
 }));
 
 type NoRoomsCardProps = {};
@@ -22,10 +30,12 @@ const NoRoomsCard = (props: NoRoomsCardProps) => {
                     </Row>
 
                     <Row>
-                        <Typography>
-                            There were no rooms found that match your filters.
-                            Please revise your filters.
-                        </Typography>
+                        <CardTextWrapper>
+                            <Typography variant={'h6'}>
+                                There were no rooms found that match your
+                                filters. Please revise your filters.
+                            </Typography>
+                        </CardTextWrapper>
                     </Row>
                 </GridContainer>
             </CustomCard>
