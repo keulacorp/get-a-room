a1c02c1bef44824b33079b69c59c38d947fed00b
Ville Pynttäri <ville.pynttari@vincit.fi>
2024-09-19 14:00:42 +0300

    Duration picker padding

--- frontend/src/components/BookingDrawer/DurationPicker.tsx
+++ frontend/src/components/BookingDrawer/DurationPicker.tsx
@@ -6,7 +6,10 @@ import { styled, Typography } from '@mui/material';
 const DurationButton = styled(ToggleButton)(() => ({}));
 
 const DurationButtonGroup = styled(ToggleButtonGroup)(() => ({
-    minWidth: '100%'
+    overflow: 'scroll',
+    width: '100%',
+    marginBottom: '8px',
+    paddingBottom: '8px'
 }));
 
 type DurationPickerProps = {
@@ -78,54 +81,52 @@ const DurationPicker = (props: DurationPickerProps) => {
     }
 
     return (
-        <div>
-            <DurationButtonGroup
-                data-testid="DurationPicker"
-                color="primary"
-                value={quickDuration}
-                exclusive
-                onChange={handleChange}
-                aria-label="duration picker"
-                fullWidth
+        <DurationButtonGroup
+            data-testid="DurationPicker"
+            color="primary"
+            value={quickDuration}
+            exclusive
+            onChange={handleChange}
+            aria-label="duration picker"
+            fullWidth
+        >
+            <DurationButton
+                data-testid="DurationPicker15"
+                value={'15'}
+                aria-label="15 minutes"
             >
-                <DurationButton
-                    data-testid="DurationPicker15"
-                    value={'15'}
-                    aria-label="15 minutes"
-                >
-                    15 min
-                </DurationButton>
-                <DurationButton
-                    data-testid="DurationPicker30"
-                    value={'30'}
-                    aria-label="30 minutes"
-                >
-                    30 min
-                </DurationButton>
-                <DurationButton
-                    data-testid="DurationPicker60"
-                    value={'60'}
-                    aria-label="1 hour"
-                >
-                    60 min
-                </DurationButton>
-                <DurationButton
-                    data-testid="DurationPicker120"
-                    value={'120'}
-                    aria-label="2 hours"
-                >
-                    120 min
-                </DurationButton>
-                {CustomDurationValueButton()}
-                <DurationButton
-                    data-testid="DurationPickerCustom"
-                    value={'Custom'}
-                    aria-label="Custom duration"
-                >
-                    Custom
-                </DurationButton>
-            </DurationButtonGroup>
-        </div>
+                15 min
+            </DurationButton>
+            <DurationButton
+                data-testid="DurationPicker30"
+                value={'30'}
+                aria-label="30 minutes"
+            >
+                30 min
+            </DurationButton>
+            <DurationButton
+                data-testid="DurationPicker60"
+                value={'60'}
+                aria-label="1 hour"
+            >
+                60 min
+            </DurationButton>
+            <DurationButton
+                data-testid="DurationPicker120"
+                value={'120'}
+                aria-label="2 hours"
+            >
+                120 min
+            </DurationButton>
+            {CustomDurationValueButton()}
+            <DurationButton
+                data-testid="DurationPickerCustom"
+                value={'Custom'}
+                aria-label="Custom duration"
+            >
+                Custom
+            </DurationButton>
+        </DurationButtonGroup>
     );
 };
 
